3852c3962694cb2d31a5eb05d87bd28e822c9a79
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2023-11-05 10:10:24 -0500

    found the last function on Stackoverflow and used it

--- script.js
+++ script.js
@@ -39,11 +39,12 @@ return password;
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  if (password) {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 }
+
 
 
 // Add event listener to generate button
