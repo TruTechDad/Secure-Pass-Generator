c4a5b60dda1a8b3d07310dea034866c813419449
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2023-11-04 22:25:26 -0400

    Added a function that prompts the length to be between 8 and 128 characters

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var lengthOfPassword = prompt("Password must be between 8 and 128 characters: Enter Now!");
+
+  if (lengthOfPassword <= 8 || lengthOfPassword >= 128)
+    alert("Password length must be a number between 8 and 128.");
+    return;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
